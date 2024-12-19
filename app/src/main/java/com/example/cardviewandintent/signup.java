1ea146490b6de87e4897c6757c4a41744e5683b7
Eman <emanhatesschool1234@gmail.com>
2024-12-19 17:54:52 +0800

    signup page and page navigation

--- app/src/main/java/com/example/cardviewandintent/signup.java
+++ app/src/main/java/com/example/cardviewandintent/signup.java
@@ -11,25 +11,24 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
-    Button signup_button;
+public class signup extends AppCompatActivity {
+    Button login_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_signup);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        signup_button = findViewById(R.id.signup);
-        signup_button.setOnClickListener(new View.OnClickListener() {
+        login_button = findViewById(R.id.login);
+        login_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, signup.class);
+                Intent intent = new Intent(signup.this, MainActivity.class);
                 startActivity(intent);
                 finish();
             }
